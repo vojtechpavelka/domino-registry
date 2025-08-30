d737906550a591fb4b176be78addbc9d977fa57f
Vojtěch Pavelka <vojtechpavelka@users.noreply.github.com>
2025-08-30 16:35:23 +0200

    Refactor registry items to change type from 'registry:component' to 'registry:ui', update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { OpenInV0Button } from "@/components/open-in-v0-button";
 import { HelloWorld } from "@/registry/domino/blocks/hello-world/hello-world";
 import { ExampleForm } from "@/registry/domino/blocks/example-form/example-form";
 import PokemonPage from "@/registry/domino/blocks/complex-component/page";
-import { ExampleCard } from "@/registry/domino/blocks/example-with-css/example-card";
 import { LoginForm } from "@/registry/domino/blocks/login-form/login-form";
 // This page displays items from the custom registry.
 // You are free to implement this with your own design as needed.
@@ -51,18 +50,6 @@ export default function Home() {
           </div>
           <div className="flex items-center justify-center min-h-[400px] relative">
             <PokemonPage />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A login form with a CSS file.
-            </h2>
-            <OpenInV0Button name="example-with-css" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
-            <ExampleCard />
           </div>
         </div>
 
