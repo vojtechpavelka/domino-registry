e3c75787c5c6774fa29756c8b908d9994378a4dc
Vojtěch Pavelka <vojtechpavelka@users.noreply.github.com>
2025-08-30 14:39:39 +0200

    Add login form block to registry

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { HelloWorld } from "@/registry/domino/blocks/hello-world/hello-world";
 import { ExampleForm } from "@/registry/domino/blocks/example-form/example-form";
 import PokemonPage from "@/registry/domino/blocks/complex-component/page";
 import { ExampleCard } from "@/registry/domino/blocks/example-with-css/example-card";
+import { LoginForm } from "@/registry/domino/blocks/login-form/login-form";
 // This page displays items from the custom registry.
 // You are free to implement this with your own design as needed.
 
@@ -62,6 +63,18 @@ export default function Home() {
           </div>
           <div className="flex items-center justify-center min-h-[400px] relative">
             <ExampleCard />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              A login form.
+            </h2>
+            <OpenInV0Button name="login-form" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <LoginForm />
           </div>
         </div>
       </main>
