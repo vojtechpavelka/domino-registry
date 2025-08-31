7582c32e16f365e92fee69ad1321474dfbefb746
Vojtěch Pavelka <vojtechpavelka@users.noreply.github.com>
2025-08-31 14:49:07 +0200

    Enhance AddCommandButton to display 'Copied to clipboard' message

--- components/add-command-button.tsx
+++ components/add-command-button.tsx
@@ -17,35 +17,40 @@ export function AddCommandButton({ registryItem }: { registryItem: string }) {
       }}
     >
       {isCopied ? (
-        <CheckIcon />
+        <>
+          <CheckIcon />
+          Copied to clipboard
+        </>
       ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
-          <rect width="256" height="256" fill="none"></rect>
-          <line
-            x1="208"
-            y1="128"
-            x2="128"
-            y2="208"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          ></line>
-          <line
-            x1="192"
-            y1="40"
-            x2="40"
-            y2="192"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          ></line>
-        </svg>
+        <>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+            <rect width="256" height="256" fill="none"></rect>
+            <line
+              x1="208"
+              y1="128"
+              x2="128"
+              y2="208"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="32"
+            ></line>
+            <line
+              x1="192"
+              y1="40"
+              x2="40"
+              y2="192"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="32"
+            ></line>
+          </svg>
+          {`@domino/${registryItem}`}
+        </>
       )}
-      {`@domino/${registryItem}`}
     </Button>
   );
 }
